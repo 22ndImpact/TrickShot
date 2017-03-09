38d6d9d6618fab7ff424228c9fbbfd0a89750d64
DESKTOP-F3CVV0E\Mitchell <1005780@student.sae.edu.au>
2017-03-09 19:19:24 +1000

    - Initial Commit, primary mechanic

---
+++ Assets/Scripts/Managers/Scr_Mgr_Game.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Mgr_Game : MonoBehaviour
+{
+    #region Variables
+    //The default scene that the core scene loads at the start of the game
+    public Scr_Mgr_Scene.Scenes DefaultScene;
+    #endregion
+
+    #region Objects
+    //Initializing a globally accessable game director
+    public static Scr_Mgr_Game inst;
+
+    #region Manager Initializations
+    [HideInInspector]
+    public Scr_Mgr_Scene Manager_Scene;
+
+    [HideInInspector]
+    public Scr_Mgr_GamePlay Manager_GamePlay;
+    #endregion
+
+    #endregion
+
+    #region Functions
+    void Awake()
+    {
+        //Setting up the bad singleton pattern
+        inst = this;
+
+        #region Manager Initializations
+        //Adds a scene manager to the GameDirectors components and sets up a public link to it
+        Manager_Scene = gameObject.AddComponent<Scr_Mgr_Scene>();
+        #endregion
+    }
+    void Start()
+    {
+        //Loads the default scene
+        Manager_Scene.LoadAdditiveScene(DefaultScene.ToString());
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Managers/Scr_Mgr_Scene.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_Mgr_Scene : MonoBehaviour
+{
+    #region Variables
+    //Use to keep track of the core scene of the game
+    public string CoreScene;
+    //Enums used to allow scene switching easier
+    public enum Scenes
+    {
+        Sce_Full_MainMenu,
+        Sce_Full_Gameplay,
+        Sce_Add_Pause,
+        Scenes_Count
+    }
+    #endregion
+
+    #region Functions
+
+    void Awake()
+    {
+        //Sets the core scene to the only scene currently active
+        CoreScene = SceneManager.GetActiveScene().name;
+    }
+
+    /// <summary>
+    /// Loads a scene replacing all other scenes current loaded.
+    /// </summary>
+    /// <param name="_SceneName">The name of the scene to load.</param>
+    public void LoadFullScene(string _SceneName)
+    {
+        SceneManager.LoadScene(_SceneName, LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// Loads a scene on top of all currently loaded scenes.
+    /// </summary>
+    /// <param name="_SceneName">The name of the scene to load.</param>
+    public void LoadAdditiveScene(string _SceneName)
+    {
+        SceneManager.LoadScene(_SceneName, LoadSceneMode.Additive);
+    }
+
+    /// <summary>
+    /// Unloads a single scene
+    /// </summary>
+    /// <param name="_SceneName">The scene to unload</param>
+    public void UnloadScene(string _SceneName)
+    {
+        SceneManager.UnloadSceneAsync(_SceneName);
+    }
+
+    /// <summary>
+    /// Unloads every scene except the scene name given
+    /// </summary>
+    /// <param name="_SceneName">The only scene left loaded, usually should be "Sce_Core"</param>
+    public void UnloadAllScenes()
+    {
+        //Unloads every scene except the core scene
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if(SceneManager.GetSceneAt(i).name != CoreScene)
+            {
+                SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+            }
+        }
+        
+    }
+
+    #endregion
+}
